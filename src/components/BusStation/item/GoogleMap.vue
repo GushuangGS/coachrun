d720d4dc211106d205718a1bdd9566aea327afbc
mystic <mystic@gotobus.com>
2019-09-10 20:07:09 +0800

    googlemapchange

--- src/components/BusStation/item/GoogleMap.vue
+++ src/components/BusStation/item/GoogleMap.vue
@@ -19,18 +19,23 @@
         var bounds = new google.maps.LatLngBounds( );//计算中心点和zoom级别
         const map = new google.maps.Map(
           document.getElementById('google-map'));
-        //   document.getElementById('google-map'), {zoom: 3, center: uluru});
+          // document.getElementById('google-map'), {zoom: 3, center: uluru});
 
         //需要显示的标记 markers
         let indexMap = undefined
+        let icon = ''
         for (let i = 0;i<this.positions.length;i++) {
           bounds.extend(new google.maps.LatLng(this.positions[i].address.latitude,this.positions[i].address.longitude));
           let pluru = {lat: this.positions[i].address.latitude, lng: this.positions[i].address.longitude}
           if (i == this.index) {
-            indexMap = new google.maps.Marker({position: pluru, icon: '/img/icon_mark.png'});//标记
+            indexMap = new google.maps.Marker({position: pluru,icon:require("./img/icon_mark.png")});//标记
             indexMap.setMap(map)
+            let infowindow = new google.maps.InfoWindow({
+              content:this.positions[this.index].name
+            });
+            infowindow.open(map,indexMap);
           } else {
-            let marker = new google.maps.Marker({position: pluru, icon: '/img/icon_mark.png'});//标记
+            let marker = new google.maps.Marker({position: pluru,icon:require("./img/icon_mark.png")});//标记
             marker.setMap(map)
           }
         }
