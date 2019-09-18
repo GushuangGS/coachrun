b5ee2acd7c7f22b9d31c225bfc373d376d185cc9
safe <safe@gotobus.com>
2019-09-18 14:46:00 +0800

    delete-router

--- src/router/user-center/user-center.js
+++ src/router/user-center/user-center.js
@@ -1,16 +1,9 @@
 import changePasswordRouter from './ChangePassword/change-password'
 import contactListRouter from './ContactInformation/contact-list'
-import editContactRouter from './ContactInformation/edit-contact'
-import addContactRouter from './ContactInformation/add-contact'
 import creditListRouter from './Credit/credit-list'
-import addCreditRouter from './Credit/add-credit'
-import editCreditRouter from './Credit/edit-credit'
 import myPointsRouter from './MyPoints/my-points'
 import myOrdersRouter from './MyOrders/my-orders'
 import myDealsRouter from './MyDeals/my-deals'
-// import resetPasswordRouter from './ResetPassword/reset-password'
-// import remindEmailRouter from './ResetPassword/remind-email'
-// import passwordRetrievalRouter from './ResetPassword/password-retrieval'
 import myBookingsRouter from './MyBookings/my-bookings'
 
 import receiverContactRouter from './ContactInformation/receiver-contact'
@@ -34,11 +27,7 @@ const userCenterRouter = {
   children: [
     changePasswordRouter,
     contactListRouter,
-    editContactRouter,
-    addContactRouter,
     creditListRouter,
-    addCreditRouter,
-    editCreditRouter,
     myPointsRouter,
     myOrdersRouter,
     myDealsRouter,
