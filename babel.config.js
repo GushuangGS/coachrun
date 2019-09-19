66058f656a54b27d5020d8a4b08122d85812d38e
safe <safe@gotobus.com>
2019-09-19 17:28:30 +0800

    update-remove-console

--- babel.config.js
+++ babel.config.js
@@ -5,23 +5,23 @@
 //   plugins.push("transform-remove-console")
 // }
 
-// let transformRemoveConsolePlugin = []
-// if (process.env.NODE_ENV !== 'development') {
-//   transformRemoveConsolePlugin = ['transform-remove-console']
-// }
+let transformRemoveConsolePlugin = []
+if (process.env.NODE_ENV !== 'development') {
+  transformRemoveConsolePlugin = ['transform-remove-console']
+}
 
 module.exports = {
   presets: [
     '@vue/app'
   ],
   plugins: [
-    // ...transformRemoveConsolePlugin,
     [
       "component",
       {
         "libraryName": "element-ui",
         "styleLibraryName": "theme-chalk"
       }
-    ]
+    ],
+    transformRemoveConsolePlugin
   ]
 }
