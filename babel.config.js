75a74df95f34d6c6b3ee22b93808a4cd8740d75c
safe <safe@gotobus.com>
2019-09-19 18:51:29 +0800

    remove-production

--- babel.config.js
+++ babel.config.js
@@ -1,28 +1,20 @@
-// // 打包时删除console
-// const plugins = [];
-// // if (process.env.NODE_ENV !== 'development') {
-// if (['production', 'sandbox'].includes(process.env.NODE_ENV)){
-//   plugins.push("transform-remove-console")
-// }
-
-const plugins = ["@vue/babel-plugin-transform-vue-jsx"]
-if(process.env.NODE_ENV === 'production') {
-  plugins.push("transform-remove-console")
+let transformRemoveConsolePlugin = ["@vue/babel-plugin-transform-vue-jsx"]
+if (process.env.NODE_ENV === 'production') {
+  transformRemoveConsolePlugin = ['transform-remove-console']
 }
 
 module.exports = {
   presets: [
     '@vue/app'
   ],
-  plugins: plugins,
-  // plugins: [
-  //   [
-  //     "component",
-  //     {
-  //       "libraryName": "element-ui",
-  //       "styleLibraryName": "theme-chalk"
-  //     }
-  //   ],
-  //   transformRemoveConsolePlugin
-  // ]
+  plugins: [
+    [
+      "component",
+      {
+        "libraryName": "element-ui",
+        "styleLibraryName": "theme-chalk"
+      }
+    ],
+    transformRemoveConsolePlugin
+  ]
 }
