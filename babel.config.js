129208473e60e46e8b08f277ba097ed468fb0426
safe <safe@gotobus.com>
2019-09-19 15:39:32 +0800

    remove-console2

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,20 @@
+// 打包时删除console
+const plugins = [];
+if (process.env.NODE_ENV !== 'development') {
+  plugins.push("transform-remove-console")
+}
+
 module.exports = {
   presets: [
     '@vue/app'
+  ],
+  plugins: [
+    [
+      "component",
+      {
+        "libraryName": "element-ui",
+        "styleLibraryName": "theme-chalk"
+      }
+    ]
   ]
 }
