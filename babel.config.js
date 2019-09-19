8433cec5b962cca2ab120c17f41c129762db6661
safe <safe@gotobus.com>
2019-09-19 16:35:28 +0800

    update-goback

--- babel.config.js
+++ babel.config.js
@@ -5,24 +5,17 @@
 //   plugins.push("transform-remove-console")
 // }
 
-let transformRemoveConsolePlugin = []
-if (process.env.NODE_ENV !== 'development') {
-  transformRemoveConsolePlugin = ['transform-remove-console']
-}
+// let transformRemoveConsolePlugin = []
+// if (process.env.NODE_ENV !== 'development') {
+//   transformRemoveConsolePlugin = ['transform-remove-console']
+// }
 
 module.exports = {
   presets: [
-    '@vue/app',
-    {
-      "useBuiltIns": "entry",
-      polyfills: [
-        'es6.promise',
-        'es6.symbol'
-      ]
-    }
+    '@vue/app'
   ],
   plugins: [
-    ...transformRemoveConsolePlugin,
+    // ...transformRemoveConsolePlugin,
     [
       "component",
       {
