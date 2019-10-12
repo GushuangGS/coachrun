e745378c889d5d0853c36b9810699a4b35d717c8
safe <safe@gotobus.com>
2019-10-12 14:26:48 +0800

    update-loading-background

--- src/main.js
+++ src/main.js
@@ -78,7 +78,7 @@ function startLoading() {//使用Element loading-start 方法
   loading = Vue.prototype.$loading({
       lock: true,
       text: 'loading……',
-      // background: 'rgba(0, 0, 0, 0.9)'
+      background: 'rgba(255, 255, 255, 0.96)'
   })
 }
 function endLoading() {//使用Element loading-close 方法
