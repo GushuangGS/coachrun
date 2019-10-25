6a3135b42cd6d3ae48e4a7150e80cadc9b8a2640
safe <safe@gotobus.com>
2019-10-25 16:17:42 +0800

    add-mybtn

---
+++ src/components/MyButton.vue
@@ -0,0 +1,33 @@
+<template>
+    <el-button :style="{width: width,height:height,backgroundColor:btnBgc,borderRadius:btnBrds}">btnTextadsss</el-button>
+</template>
+
+<script>
+    export default{
+        name:'myButton',
+        props:{
+            width:{
+                type:String,
+                default:'100px'
+            },
+            height:{
+                type:String,
+                default:'40px'
+            },
+            btnBgc:{
+                type:String,
+                default:'#FFFFFF'
+            },
+            btnBrds:{
+                type:String,
+                default:'4px'
+            }
+        }
+    }
+
+</script>
+
+<style>
+
+
+</style>
