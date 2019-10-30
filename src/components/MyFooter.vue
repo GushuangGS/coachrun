f566ae512ad6d224ef5072cad89ed4cdee66ff1f
safe <safe@gotobus.com>
2019-10-30 19:11:23 +0800

    change-footer-bg

--- src/components/MyFooter.vue
+++ src/components/MyFooter.vue
@@ -4,7 +4,7 @@
             <el-row :gutter="24">
                 <el-col :xs="6" :sm="6">
                     <div class="bot-logo">
-                        <img src="@/assets/whitecoachrunlogo.png" alt="">
+                        <img src="@/assets/coachrunlogo.png" alt="">
                     </div>
                 </el-col>
                 <el-col :xs="6" :sm="6">
@@ -56,13 +56,13 @@
 <style lang="scss" scoped>
     .net-link {
         text-decoration: none;
-        color: white;
+        color: #333;
     }
     .net-link:hover{
-        color: #22ffff;
+        color: #009FEA;
     }
     .footer{
-        background: #00A2FF;
+        background: #f5f5f5;
     }
     .footer-top{
       height: 232px;
@@ -71,7 +71,7 @@
       display: flex;
       flex-direction: column;
       font-size: $Body2Size;
-      color:rgba(255,255,255,1);
+      color:#333;
       line-height: 20px;
       margin: 0 auto;
       padding-top: 40px;
@@ -81,7 +81,7 @@
       margin-left: 14px;
     }
     .bot-title{
-      color:rgba(255,255,255,1);
+      color:#333;
       font-size:$Body1Size;
       font-weight: bold;
       margin-bottom: 20px;
@@ -93,7 +93,7 @@
     }
     .bot-link a{
       text-decoration: none;
-      color:rgba(255,255,255,1);
+      color:#333;
       font-size:$Body2Size;
       line-height:28px;
     }
@@ -120,11 +120,11 @@
         height: 43px;
         text-align: center;
         font-weight:400;
-        color:rgba(255,255,255,1);
+        color:#333;
         line-height: 43px;
         font-size: $Body3Size;
       padding-right: 3px;
-        box-shadow: 0px 1px 2px 0px rgba(255,255,255,1)
+        box-shadow: 0px 1px 2px 0px #333;
     }
     @media screen and (max-width:768px){
         .footer-top {
