c2ee150631407694315c353d3e149c9ab256636c
safe <safe@gotobus.com>
2019-12-03 19:17:47 +0800

    change-fcgi

--- src/configs/siteConfig.js
+++ src/configs/siteConfig.js
@@ -1,6 +1,6 @@
 export function websiteDomain(path){
     let domain = ''
-    if(process.env === 'production'){
+    if(process.env.NODE_ENV === 'production'){
         domain = `${process.env.VUE_APP_WEBSITE_DOMAIN}/cgi-bin/${path}.fcgi`
     }else{
         domain = `${process.env.VUE_APP_WEBSITE_DOMAIN}/cgi-bin/${path}.cgi`
